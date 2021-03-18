b39c184a65d8c4dbf27fd2e925de588fe2629892
Jakub Polomsky <jakub.polomsky@qenta.com>
2021-03-18 09:41:53 +0100

    rename language files and re-brand the content

---
+++ admin/language/en-gb/extension/payment/qenta_invoice.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by QENTA Payment CEE GmbH
+ * (abbreviated to QENTA) and are explicitly not part of the QENTA
+ * CEE range of products and services.
+ *
+ * They have been tested and approved for full functionality in the standard
+ * configuration
+ * (status on delivery) of the corresponding shop system. They are under
+ * General Public License Version 2 (GPLv2) and can be used, developed and
+ * passed on to third parties under the same terms.
+ *
+ * However, QENTA does not provide any guarantee or accept any liability
+ * for any errors occurring when used in an enhanced, customized shop system
+ * configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and
+ * requires a comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. QENTA does not guarantee
+ * their full functionality neither does QENTA assume liability for any
+ * disadvantages related to the use of the plugins. Additionally, QENTA
+ * does not guarantee the full functionality for customized shop systems or
+ * installed plugins of other vendors of plugins within the same shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before
+ * starting productive operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these
+ * terms of use. Please do not use the plugin if you do not agree to these
+ * terms of use!
+ */
+
+$_['heading_title'] = 'QENTA Checkout Page Invoice';
+$_['text_qenta_invoice'] = '<a href="http://www.qenta-cee.at/" target="_blank"><img src="view/image/payment/qenta.png" alt="QENTA" title="QENTA" /></a>';
+
+$_['provider_title'] = 'Invoice Provider';
+$_['provider_descript'] = 'Select your invoice provider';
+
+$_['country_title'] = 'Allowed countries';
+$_['country_descript'] = 'Select allowed countries for invoice';
+
+$_['currency_title'] = 'Accepted currencies';
+$_['currency_descript'] = 'Select accepted currencies for invoice';
+
+$_['mId_title'] = 'Payolution mID';
+$_['mId_descript'] = 'Your payolution merchant ID, non-base64-encoded.';
+
+$_['terms_title'] = 'Payolution terms';
+$_['terms_descript'] = 'Consumer must accept payolution terms during the checkout process.';
